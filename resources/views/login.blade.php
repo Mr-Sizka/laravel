23e5aa63b023683f3dd4423304f13e60ff087cfe
Sisuka Udawatta <works.sisuka@gmail.com>
2024-07-05 10:58:36 +0530

    remove unused div in login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,34 +2,6 @@
 
 @section('title', 'LogIn')
 @section('content')
-<!--<div class="container ">
-    <div class="text-center">
-        <div class="row justify-content-center">
-            <div class="col-md-3">
-            <h1>Login</h1>
-                @foreach($errors as $error)
-                <div class="alert alert-danger" role="alert">
-                    {{$error}}
-                </div>
-                @endforeach
-            <form action="/login" method="post" class="form-control">
-                @csrf
-                <div class="mb-3">
-                    <input type="email" name="email" required placeholder="Email">
-                </div>
-                <div class="mb-3">
-                    <input type="password" name="password" required placeholder="password">
-                </div>
-                <button type="submit" class="btn btn-primary">LogIn</button>
-                <button type="reset" class="btn btn-warning"> clear</button>
-                <div >
-                    <a href="/register" >Register</a>
-                </div>
-            </form>
-            </div>
-        </div>
-    </div>
-</div>-->
 
 <div class="container d-flex justify-content-center align-items-center min-vh-100">
     <div class="row rounded-5 bg-white border flex-column shadow p-3">
